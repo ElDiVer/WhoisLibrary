643a5ecf93d6a71add2e8bfdb451edeb8917f90e
Ivan Gualandri <ivan.gualandri@gmail.com>
2014-03-09 03:55:52 +0100

    Working in It domain class

--- src/org/whoislibrary/servers/WhoisIt.java
+++ src/org/whoislibrary/servers/WhoisIt.java
@@ -1,6 +1,8 @@
 package org.whoislibrary.servers;
 
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.Date;
 
 import org.whoislibrary.Whois;
 import org.whoislibrary.WhoisAbstract;
@@ -15,7 +17,18 @@ public class WhoisIt extends WhoisAbstract implements Whois {
 
 	@Override
 	public WhoisEntry parseResponse(BufferedReader queryResult) {
+		String queryLine;
+		Date expDate = null;
 		System.out.println(queryResult.toString());
+		try {
+			while ((queryLine = queryResult.readLine()) != null) {
+				if(queryLine.contains("Registry Expiry Date:")){
+					String expString = queryLine.replace("Expire Date:", "").trim();
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		return null;
 	}
 
