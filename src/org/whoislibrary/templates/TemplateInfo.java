3319ce34636f6ce6f53a93b25b7db42dfc832b62
Ivan Gualandri <ivan.gualandri@gmail.com>
2015-06-07 11:04:03 +0100

    Add parse operation for info domain.

--- src/org/whoislibrary/templates/TemplateInfo.java
+++ src/org/whoislibrary/templates/TemplateInfo.java
@@ -1,8 +1,32 @@
 package org.whoislibrary.templates;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
+import org.whoislibrary.parser.DateOperation;
+import org.whoislibrary.parser.ParseOperation;
+import org.whoislibrary.parser.SkipOperation;
 import org.whoislibrary.parser.Template;
 
+/**
+ * 
+ * Template class to parse the whois data for .info domains.
+ *  
+ * @author Ivan Gualandri
+ * @version 1.0
+ *
+ */
 public class TemplateInfo extends Template {
+	private static final DateFormat dateFormat
+	= new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH);
+	
+	private static final ParseOperation parse[] = {
+		new SkipOperation(2),
+		new DateOperation("Creation Date: ", CREATION, dateFormat),
+		new DateOperation("Updated Date: ", LASTUPDATED, dateFormat),
+		new DateOperation("Registry Expiry Date: ", EXPIRE, dateFormat)
+	};
 
 	public TemplateInfo() {
 		super("whois.afilias.net");
