ae23347675de13044e3701763c01a83ea242b5b3
Ivan Gualandri <ivan.gualandri@gmail.com>
2015-06-07 10:53:33 +0100

    Add .info suffix class.

---
+++ src/org/whoislibrary/templates/TemplateInfo.java
@@ -0,0 +1,12 @@
+package org.whoislibrary.templates;
+
+import org.whoislibrary.parser.Template;
+
+public class TemplateInfo extends Template {
+
+	public TemplateInfo() {
+		super("whois.afilias.net");
+		// TODO Auto-generated constructor stub
+	}
+
+}
